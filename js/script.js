a8096557d2eac8678667e772df8821af4177f39a
Kevin Soffa <84532185+KevinSoffa@users.noreply.github.com>
2022-06-04 15:58:48 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -12,9 +12,27 @@ const jump = () => {
     }, 500);
 }
 
+//Lógica do pulo do Mario
 const loop = setInterval(() => {
 
-    const Pipeposition = 
+    
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition < 125 && pipePosition > 0 && marioPosition < 50) { //condição para prosseguir no jogo
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`; 
+
+        pipe.style.animation = 'none';
+        pipe.style.bottom = `${marioPosition}px`; 
+
+        mario.src = './images/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+    }
 
 }, 10)
 
