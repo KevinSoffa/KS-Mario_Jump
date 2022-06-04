c7f5b3bd3f8b6f508bcee123492b8eb31cb59ada
Kevin Soffa <84532185+KevinSoffa@users.noreply.github.com>
2022-06-04 01:10:24 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe')
+
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump')
+
+    }, 500);
+}
